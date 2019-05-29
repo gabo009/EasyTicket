c6086343d5e276e37a2f3b8a202842233ffe748b
Gabriel Herrera <gabriel.herrera12@inacapmail.cl>
2019-05-28 21:59:58 -0400

    coneccion hecha

---
+++ EasyTicket/Repositorio/Clase1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorio
+{
+    [Table("clase1")]
+    public class Clase1
+    {
+        [Key]
+        [Column("Codigo")]
+        public int Codigo { get; set; }
+        [Column("Numero")]
+        public int Numero { get; set; }
+
+    }
+}
---
+++ EasyTicket/Repositorio/context.cs
@@ -0,0 +1,18 @@
+﻿using MySql.Data.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorio
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public partial class db_Entities : DbContext
+    {
+        public db_Entities() : base(nameOrConnectionString: "MyContext") { }
+
+        public DbSet<Clase1> Clase1 { get; set; }
+    }
+}
